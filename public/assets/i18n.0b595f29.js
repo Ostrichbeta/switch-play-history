6bc52fba04cfa0f2505426db1c42d95a43e08687
ostrichb <ostrichb@outlook.com>
2024-08-08 06:27:24 +0000

    Language code fix

--- public/assets/i18n.0b595f29.js
+++ public/assets/i18n.0b595f29.js
@@ -1,4 +1,4 @@
-import{d as Nt,h as sn,e as wn,c as le,w as it,F as cn,i as Un,o as Wn,a as $n,s as on,r as pe,b as xn,f as Hn,g as qe,j as Gn,T as Vn,k as Yn}from"./index.5cce1395.js";/*!
+import{d as Nt,h as sn,e as wn,c as le,w as it,F as cn,i as Un,o as Wn,a as $n,s as on,r as pe,b as xn,f as Hn,g as qe,j as Gn,T as Vn,k as Yn}from"./index.8be3872f.js";/*!
   * shared v9.13.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
